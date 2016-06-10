574ba5729d6be9e8d53985ae9d2b908bd0ba1a26
DaniOrtiz <danidaniortiz@gmail.com>
2016-06-10 05:59:19 -0430

    luces y colores, falta filtro bilineal y mix piso

---
+++ Plantilla_Proyecto_Texture_Shader01/PlantillaOpenGL01/mainShader01.cpp
@@ -0,0 +1,560 @@
+// Cubica
+
+#include <stdlib.h>
+#include <conio.h>
+
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+#include <iostream>
+#include "glsl.h"
+#include "glm.h"
+
+// assimp include files. These three are usually needed.
+#include <assimp/cimport.h>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+// the global Assimp scene object
+const aiScene* scene = NULL;
+GLuint scene_list = 0;
+aiVector3D scene_min, scene_max, scene_center;
+
+#define aisgl_min(x,y) (x<y?x:y)
+#define aisgl_max(x,y) (y>x?y:x)
+
+
+using namespace std;
+
+cwc::glShaderManager SM;
+cwc::glShader *shader;
+
+
+//Textures
+static GLuint texflat;
+unsigned char* imageflat = NULL;
+
+//Central
+static GLuint texcentral;
+unsigned char* imagecentral = NULL;
+
+//Relleno 01
+static GLuint texrelleno01;
+unsigned char* imagerelleno01 = NULL;
+
+//Relleno 02
+static GLuint texrelleno02;
+unsigned char* imagerelleno02 = NULL;
+
+//Variables para la intensidad de las luces
+//El texture.frag lanza error si coloco GLfloat
+float ambiental = 1.0;	// Valores en los que se pide iniciar el programa
+float central = 1.0;	
+float relleno01 = 0.0;	// Al iniciar no se pueden reducir las luces de relleno (estan apagadas)
+float relleno02  = 0.0;
+
+//Variables para el color de las luces
+float colorA[4] = {1.0,1.0,1.0,1.0}; // Para RGB solo necesito 3 pero para el texture.frag 4
+float colorC[4] = {1.0,1.0,1.0,1.0};
+float colorR01[4] = {1.0,1.0,1.0,1.0};
+float colorR02[4] = {1.0,1.0,1.0,1.0};
+
+int iheight, iwidth;
+
+
+void ejesCoordenada() {
+	
+	glDisable(GL_LIGHTING);	
+	glLineWidth(2.5);
+	glBegin(GL_LINES);
+		glColor3f(1.0,0.0,0.0);
+		glVertex2f(0,10);
+		glVertex2f(0,-10);
+		glColor3f(0.0,0.0,1.0);
+		glVertex2f(10,0);
+		glVertex2f(-10,0);
+	glEnd();
+
+	glLineWidth(1.5);
+	int i;
+	glColor3f(0.0,1.0,0.0);
+	glBegin(GL_LINES);
+		for(i = -10; i <=10; i++){
+			if (i!=0) {		
+				if ((i%2)==0){	
+					glVertex2f(i,0.4);
+					glVertex2f(i,-0.4);
+
+					glVertex2f(0.4,i);
+					glVertex2f(-0.4,i);
+				}else{
+					glVertex2f(i,0.2);
+					glVertex2f(i,-0.2);
+
+					glVertex2f(0.2,i);
+					glVertex2f(-0.2,i);
+
+				}
+			}
+		}
+		
+	glEnd();
+
+	glEnable(GL_LIGHTING);
+
+	glLineWidth(1.0);
+}
+
+void changeViewport(int w, int h) {
+	
+	float aspectratio;
+
+	if (h==0)
+		h=1;
+
+	
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity ();
+   gluPerspective(55, (GLfloat) w/(GLfloat) h, 1.0, 300.0);
+   glMatrixMode (GL_MODELVIEW);
+
+}
+
+void init(){
+	
+   glEnable(GL_LIGHTING);
+   glEnable(GL_LIGHT0);
+   glEnable(GL_DEPTH_TEST);
+   
+  
+   // Cargando Textura
+   glGenTextures(1, &texflat);
+   glBindTexture(GL_TEXTURE_2D, texflat);
+
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+   imageflat = glmReadPPM("baked_flat.ppm", &iwidth, &iheight);
+   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, iwidth, iheight, 0, GL_RGB, GL_UNSIGNED_BYTE, imageflat);
+
+   // Luz central
+   glGenTextures(1, &texcentral);
+   glBindTexture(GL_TEXTURE_2D, texcentral);
+
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+   imagecentral = glmReadPPM("baked_keyrabbit.ppm", &iwidth, &iheight);
+   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, iwidth, iheight, 0, GL_RGB, GL_UNSIGNED_BYTE, imagecentral);
+
+   // Luz relleno 01
+   glGenTextures(1, &texrelleno01);
+   glBindTexture(GL_TEXTURE_2D, texrelleno01);
+
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+   imagerelleno01 = glmReadPPM("baked_flat.ppm", &iwidth, &iheight);
+   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, iwidth, iheight, 0, GL_RGB, GL_UNSIGNED_BYTE, imagerelleno01);
+
+   // Luz relleno 02
+   glGenTextures(1, &texrelleno02);
+   glBindTexture(GL_TEXTURE_2D, texrelleno02);
+
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+   imagerelleno02 = glmReadPPM("baked_fill01.ppm", &iwidth, &iheight);
+   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, iwidth, iheight, 0, GL_RGB, GL_UNSIGNED_BYTE, imagerelleno02);
+
+   shader = SM.loadfromFile("texture.vert","texture.frag"); // load (and compile, link) from file
+  		  if (shader==0) 
+			  std::cout << "Error Loading, compiling or linking shader\n";
+
+}
+
+
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	// ---------- INTENSIDAD LUCES ----------
+	// incrementa intensidad luz ambiental
+	if (key == '1'){
+		ambiental += 0.05;
+	}
+	// reduce intensidad luz ambiental
+	if (key == '2'){
+		if (ambiental - 0.05 > 0.0) ambiental -= 0.05;
+	}
+	// incrementa intensidad luz relleno 01
+	if (key == 'q'){
+		relleno01 += 0.05;
+	}
+	// reduce intensidad luz relleno 01
+	if (key == 'w'){
+		if (relleno01 - 0.05 > 0.0) relleno01 -= 0.05;
+	}
+	// incrementa intensidad luz relleno 02
+	if (key == 'a'){
+		relleno02 += 0.05;
+	}
+	// reduce intensidad luz relleno 02
+	if (key == 's'){
+		if (relleno02 - 0.05 > 0.0) relleno02 -= 0.05;
+	}
+	// incrementa intensidad luz central
+	if (key == 'z'){
+		central += 0.05;
+	}
+	// reduce intensidad luz central
+	if (key == 'x'){
+		if (central - 0.05 > 0.0) central -= 0.05;
+	}
+	// ---------- COLOR LUCES ----------
+	// incrementos color luz relleno 01
+	// componente R
+	if (key == 'e'){
+		colorR01[0] += 0.05;
+	}
+	// componente G
+	if (key == 'r'){
+		colorR01[1] += 0.05;
+	}
+	// componente B
+	if (key == 't'){
+		colorR01[2] += 0.05;
+	}
+	// reducciones color luz relleno 01
+	// componente R
+	if (key == 'y'){
+		if (colorR01[0] - 0.05 > 0.0) colorR01[0] -= 0.05;
+	}
+	// componente G
+	if (key == 'u'){
+		if (colorR01[1] - 0.05 > 0.0) colorR01[1] -= 0.05;
+	}
+	// componente B
+	if (key == 'i'){
+		if (colorR01[2] - 0.05 > 0.0) colorR01[2] -= 0.05;
+	}
+	// incrementos color luz relleno 02
+	// componente R
+	if (key == 'd'){
+		colorR02[0] += 0.05;
+	}
+	// componente G
+	if (key == 'f'){
+		colorR02[1] += 0.05;
+	}
+	// componente B
+	if (key == 'g'){
+		colorR02[2] += 0.05;
+	}
+	// reducciones color luz relleno 02
+	// componente R
+	if (key == 'h'){
+		if (colorR02[0] - 0.05 > 0.0) colorR02[0] -= 0.05;
+	}
+	// componente G
+	if (key == 'j'){
+		if (colorR02[1] - 0.05 > 0.0) colorR02[1] -= 0.05;
+	}
+	// componente B
+	if (key == 'k'){
+		if (colorR02[2] - 0.05 > 0.0) colorR02[2] -= 0.05;
+	}
+	// incrementos color luz central
+	// componente R
+	if (key == 'c'){
+		colorC[0] += 0.05;
+	}
+	// componente G
+	if (key == 'v'){
+		colorC[1] += 0.05;
+	}
+	// componente B
+	if (key == 'b'){
+		colorC[2] += 0.05;
+	}
+	// reducciones color luz central
+	// componente R
+	if (key == 'n'){
+		if (colorC[0] - 0.05 > 0.0) colorC[0] -= 0.05;
+	}
+	// componente G
+	if (key == 'm'){
+		if (colorC[1] - 0.05 > 0.0) colorC[1] -= 0.05;
+	}
+	// componente B
+	if (key == ' '){
+		if (colorC[2] - 0.05 > 0.0) colorC[2] -= 0.05;
+	}
+	// ---------- FILTROS BILINEALES ----------
+	/*
+	// activacion
+	if (key == 'o'){
+	}
+	// desactivacion
+	if (key == 'p'){
+	}
+	// ---------- COLORES PATRON PISO ----------
+	if (key == '3'){
+	}
+	if (key == '4'){
+	}
+	if (key == '5'){
+	}
+	if (key == '6'){
+	}
+	*/
+	switch (key)
+	{
+		default:
+			break;
+	}
+
+	glutPostRedisplay();
+}
+
+void recursive_render (const aiScene *sc, const aiNode* nd)
+{
+	unsigned int i;
+	unsigned int n = 0, t;
+	aiMatrix4x4 m = nd->mTransformation;
+
+	// update transform
+	aiTransposeMatrix4(&m);
+	glPushMatrix();
+	glMultMatrixf((float*)&m);
+
+	// draw all meshes assigned to this node
+	for (; n < nd->mNumMeshes; ++n) {
+		
+		const aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
+
+		for (t = 0; t < mesh->mNumFaces; ++t) {
+			const aiFace* face = &mesh->mFaces[t];
+			GLenum face_mode;
+
+			switch(face->mNumIndices) {
+				case 1: face_mode = GL_POINTS; break;
+				case 2: face_mode = GL_LINES; break;
+				case 3: face_mode = GL_TRIANGLES; break;
+				default: face_mode = GL_POLYGON; break;
+			}
+
+			glBegin(face_mode);
+
+			for(i = 0; i < face->mNumIndices; i++) {
+				int index = face->mIndices[i];
+				
+				if(mesh->mColors[0] != NULL)
+					glColor4fv((GLfloat*)&mesh->mColors[0][index]);
+				
+				if(mesh->mNormals != NULL) 
+					glNormal3fv(&mesh->mNormals[index].x);
+				
+				if (mesh->HasTextureCoords(0)) 
+					glTexCoord2f(mesh->mTextureCoords[0][index].x, 1-mesh->mTextureCoords[0][index].y);
+				
+				glVertex3fv(&mesh->mVertices[index].x);
+			}
+
+			glEnd();
+		}
+
+	}
+
+	// draw all children
+	for (n = 0; n < nd->mNumChildren; ++n) {		
+		recursive_render(sc, nd->mChildren[n]);
+	}
+
+	glPopMatrix();
+}
+
+void render(){
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	GLfloat zExtent, xExtent, xLocal, zLocal;
+    int loopX, loopZ;
+
+	glLoadIdentity ();                       
+	gluLookAt (0.0, 37.0, 98.0, 0.0, 30.0, 0.0, 0.0, 1.0, 0.0);
+	
+	glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable( GL_LINE_SMOOTH );	
+
+			
+	glPushMatrix();
+
+	if (shader) shader->begin();
+
+	// Texturas
+	shader->setTexture("stexflat", texflat,0);
+	shader->setTexture("stexcentral", texcentral,1);
+	shader->setTexture("stexrelleno01", texrelleno01,2);
+	shader->setTexture("stexrelleno02", texrelleno02,3);
+
+	// Intensidad Luces
+	shader->setUniform1f("ambientalInt",ambiental);
+	shader->setUniform1f("centralInt",central);
+	shader->setUniform1f("relleno01Int",relleno01);
+	shader->setUniform1f("relleno02Int",relleno02);
+
+	// Color Luces
+	shader->setUniform4f("ambientalColor",colorA[0],colorA[1],colorA[2],colorA[3]);
+	shader->setUniform4f("centralColor",colorC[0],colorC[1],colorC[2],colorC[3]);
+	shader->setUniform4f("relleno01Color",colorR01[0],colorR01[1],colorR01[2],colorR01[3]);
+	shader->setUniform4f("relleno02Color",colorR02[0],colorR02[1],colorR02[2],colorR02[3]);
+
+
+	// Codigo para el mesh	
+	glEnable(GL_NORMALIZE);
+	glTranslatef(0.0, -2.0, 0.0);
+	glRotatef(0.0, 0.0, 1.0, 0.0);
+	glScalef(1.0, 1.0, 1.0);
+	if(scene_list == 0) {
+	    scene_list = glGenLists(1);
+	    glNewList(scene_list, GL_COMPILE);
+            // now begin at the root node of the imported data and traverse
+            // the scenegraph by multiplying subsequent local transforms
+            // together on GL's matrix stack.
+	    recursive_render(scene, scene->mRootNode);
+	    glEndList();
+	}
+	glCallList(scene_list);
+	
+	glPopMatrix();
+	
+	
+	/*
+	glPushMatrix();
+	glLoadIdentity();	
+	glTranslatef(1.1, 0.5, -3.0);
+	glutSolidSphere(0.2f,30,30);
+	glPopMatrix();*/
+	 
+
+	if (shader) shader->end();
+	
+	glDisable(GL_BLEND);
+	glDisable(GL_LINE_SMOOTH);
+	glutSwapBuffers();
+}
+
+void animacion(int value) {
+	
+	glutTimerFunc(10,animacion,1);
+    glutPostRedisplay();
+	
+}
+
+void get_bounding_box_for_node (const aiNode* nd, 	aiVector3D* min, 	aiVector3D* max, 	aiMatrix4x4* trafo){
+	aiMatrix4x4 prev;
+	unsigned int n = 0, t;
+
+	prev = *trafo;
+	aiMultiplyMatrix4(trafo,&nd->mTransformation);
+
+	for (; n < nd->mNumMeshes; ++n) {
+		const aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
+		for (t = 0; t < mesh->mNumVertices; ++t) {
+
+			aiVector3D tmp = mesh->mVertices[t];
+			aiTransformVecByMatrix4(&tmp,trafo);
+
+			min->x = aisgl_min(min->x,tmp.x);
+			min->y = aisgl_min(min->y,tmp.y);
+			min->z = aisgl_min(min->z,tmp.z);
+
+			max->x = aisgl_max(max->x,tmp.x);
+			max->y = aisgl_max(max->y,tmp.y);
+			max->z = aisgl_max(max->z,tmp.z);
+		}
+	}
+
+	for (n = 0; n < nd->mNumChildren; ++n) {
+		get_bounding_box_for_node(nd->mChildren[n],min,max,trafo);
+	}
+	*trafo = prev;
+}
+
+void get_bounding_box (aiVector3D* min, aiVector3D* max){
+	aiMatrix4x4 trafo;
+	aiIdentityMatrix4(&trafo);
+	
+	min->x = min->y = min->z =  1e10f;
+	max->x = max->y = max->z = -1e10f;
+	get_bounding_box_for_node(scene->mRootNode,min,max,&trafo);
+}
+
+int loadasset (const char* path){
+	// we are taking one of the postprocessing presets to avoid
+	// spelling out 20+ single postprocessing flags here.
+	scene = aiImportFile(path,aiProcessPreset_TargetRealtime_MaxQuality);
+
+	if (scene) {
+		get_bounding_box(&scene_min,&scene_max);
+		scene_center.x = (scene_min.x + scene_max.x) / 2.0f;
+		scene_center.y = (scene_min.y + scene_max.y) / 2.0f;
+		scene_center.z = (scene_min.z + scene_max.z) / 2.0f;
+		return 0;
+	}
+	return 1;
+}
+
+int main (int argc, char** argv) {
+
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+
+	glutInitWindowSize(960,540);
+
+	glutCreateWindow("Test Opengl");
+
+
+	// Codigo para cargar la geometria usando ASSIMP
+
+	aiLogStream stream;
+	// get a handle to the predefined STDOUT log stream and attach
+	// it to the logging system. It remains active for all further
+	// calls to aiImportFile(Ex) and aiApplyPostProcessing.
+	stream = aiGetPredefinedLogStream(aiDefaultLogStream_STDOUT,NULL);
+	aiAttachLogStream(&stream);
+
+	// ... same procedure, but this stream now writes the
+	// log messages to assimp_log.txt
+	stream = aiGetPredefinedLogStream(aiDefaultLogStream_FILE,"assimp_log.txt");
+	aiAttachLogStream(&stream);
+
+	// the model name can be specified on the command line. If none
+	// is specified, we try to locate one of the more expressive test 
+	// models from the repository (/models-nonbsd may be missing in 
+	// some distributions so we need a fallback from /models!).
+	
+	
+	loadasset( "escenario_proyecto01.obj");
+
+
+
+	init ();
+
+	glutReshapeFunc(changeViewport);
+	glutDisplayFunc(render);
+	glutKeyboardFunc (Keyboard);
+	
+	glutMainLoop();
+	return 0;
+
+}
---
+++ Plantilla_Proyecto_Texture_Shader01/PlantillaOpenGL01/texture.frag
@@ -0,0 +1,71 @@
+// Definiciones para el Filtro Bilineal
+#define textureWidth 600.0
+#define textureHeight 800.0	
+#define texel_size_x 1.0 / textureWidth
+#define texel_size_y 1.0 / textureHeight
+
+// Texturas
+uniform sampler2D stexflat;
+uniform sampler2D stexcentral;
+uniform sampler2D stexrelleno01;
+uniform sampler2D stexrelleno02;
+
+// Intensidad Luces
+uniform float ambientalInt;
+uniform float centralInt;
+uniform float relleno01Int;
+uniform float relleno02Int;
+
+// Color Luces
+uniform vec4 ambientalColor;
+uniform vec4 centralColor;
+uniform vec4 relleno01Color;
+uniform vec4 relleno02Color;
+
+// Algoritmo Filtro Bilineal
+vec4 texture2D_bilinear( sampler2D tex, vec2 uv )
+{
+	vec2 f;
+
+	f.x	= fract( uv.x * textureWidth );
+	f.y	= fract( uv.y * textureHeight );
+
+	vec4 t00 = texture2D( tex, uv + vec2( 0.0, 0.0 ));
+	vec4 t10 = texture2D( tex, uv + vec2( texel_size_x, 0.0 ));
+	vec4 tA = mix( t00, t10, f.x);
+
+	vec4 t01 = texture2D( tex, uv + vec2( 0.0, texel_size_y ) );
+	vec4 t11 = texture2D( tex, uv + vec2( texel_size_x, texel_size_y ) );
+	vec4 tB = mix( t01, t11, f.x );
+
+	return mix( tA, tB, f.y );
+}
+
+void main(void) {
+
+	vec4 cFinal;
+	vec4 cAmbiental;
+	vec4 cCentral;
+	vec4 cRelleno01;
+	vec4 cRelleno02;
+	vec4 auxAmbiental;
+	vec4 auxCentral;
+	vec4 auxRelleno01;
+	vec4 auxRelleno02;
+
+	cAmbiental = texture2D(stexflat,gl_TexCoord[0].st);
+	cCentral = texture2D(stexcentral,gl_TexCoord[0].st);
+	cRelleno01 = texture2D(stexrelleno01,gl_TexCoord[0].st);
+	cRelleno02 = texture2D(stexrelleno02,gl_TexCoord[0].st);
+
+	auxAmbiental = cAmbiental * ambientalInt * ambientalColor;
+	auxCentral = cCentral * centralInt * centralColor;
+	auxRelleno01 = cRelleno01 * relleno01Int * relleno01Color;
+	auxRelleno02 = cRelleno02 * relleno02Int * relleno02Color;
+	
+	cFinal = auxAmbiental * (auxCentral + auxRelleno01 + auxRelleno02);
+	gl_FragColor = cFinal;
+
+	// filtro bilinear
+	// gl_FragColor = texture2D_bilinear( colorMap, gl_TexCoord[0].st);
+}
